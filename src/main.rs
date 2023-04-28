3b1c7aadf62451704872768adce9dcbb84f6a6ca
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-04-28 18:54:37 +0000

    sql query test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 extern crate actix_web;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-// use std::env;
-// use serde_json::value::Value;
-use std::fs;
-// use aws_sdk_s3::{config, Client, Error};
-// use polars::prelude::*;
-// use datafusion_objectstore_s3::object_store::s3::S3FileSystem;
-// use std::sync::Arc;
-// use datafusion::datasource::listing::*;
-// use datafusion::prelude::SessionContext;
+
+// use std::fs;
+// use aws_sdk_s3::Client;
+use std::env;
+
+use aws_sdk_s3::types::{
+    CompressionType, CsvInput, CsvOutput, ExpressionType, FileHeaderInfo, InputSerialization,
+    OutputSerialization, SelectObjectContentEventStream,
+};
+use aws_sdk_s3::Client;
+
+use polars::prelude::*;
+
 
 
 
@@ -19,35 +23,35 @@ async fn hello() -> impl Responder {
 
 //function that plots the team results
 #[get("/team_results")]
-async fn team_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
-    // async fn team_result_plot() -> impl Responder {
-    const TEAM_RES: &str = "processed-data/model_res_teams.csv";
-    const TEAM_RES_PNG: &str = "team_res.png";
-    final_project::plot_res(TEAM_RES,TEAM_RES_PNG).await;
+// async fn team_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
+    async fn team_result_plot() -> impl Responder {
+    // const TEAM_RES: &str = "processed-data/model_res_teams.csv";
+    // const TEAM_RES_PNG: &str = "team_res.png";
+    // final_project::plot_res(TEAM_RES,TEAM_RES_PNG).await;
 
-    // run the plot function and show plot on the actix server
-    let image_data = fs::read(TEAM_RES_PNG)?;
-    Ok(HttpResponse::Ok()
-        .content_type("image/png")
-        .body(image_data))
+    // // run the plot function and show plot on the actix server
+    // let image_data = fs::read(TEAM_RES_PNG)?;
+    // Ok(HttpResponse::Ok()
+    //     .content_type("image/png")
+    //     .body(image_data))
 
-    // HttpResponse::Ok().body("Other test!")
+    HttpResponse::Ok().body("Other test!")
 }
 
 //function that plots the player results
 #[get("/player_results")]
-async fn player_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
-// async fn player_result_plot() -> impl Responder {
-    const PLAY_RES: &str = "processed-data/model_res_players.csv";
-    const PLAY_RES_PNG: &str = "player_res.png";
-    final_project::plot_res(PLAY_RES,PLAY_RES_PNG).await;
+// async fn player_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
+async fn player_result_plot() -> impl Responder {
+    // const PLAY_RES: &str = "processed-data/model_res_players.csv";
+    // const PLAY_RES_PNG: &str = "player_res.png";
+    // final_project::plot_res(PLAY_RES,PLAY_RES_PNG).await;
 
-    // run the plot function and show plot on the actix server
-    let image_data = fs::read(PLAY_RES_PNG)?;
-    Ok(HttpResponse::Ok()
-        .content_type("image/png")
-        .body(image_data))
-    // HttpResponse::Ok().body("Test!")
+    // // run the plot function and show plot on the actix server
+    // let image_data = fs::read(PLAY_RES_PNG)?;
+    // Ok(HttpResponse::Ok()
+    //     .content_type("image/png")
+    //     .body(image_data))
+    HttpResponse::Ok().body("Test!")
 }
 
 
@@ -62,61 +66,113 @@ async fn player_result_plot() -> Result<HttpResponse, actix_web::error::ParseErr
 // }
 
 
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     //add a print message to the console that the service is running
-//     println!("Starting service...");
-//     HttpServer::new(|| App::new().service(team).service(player))
-//         .bind("0.0.0.0:8080")?
-//         .run()
-//         .await
-// }
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // let AWS_S3_BUCKET = env::var("AWS_S3_BUCKET").expect("AWS_S3_BUCKET must be set");
-    // let AWS_ACCESS_KEY_ID = env::var("AWS_ACCESS_KEY_ID");
-    // let AWS_SECRET_ACCESS_KEY = env::var("AWS_SECRET_ACCESS_KEY");
-    // let config = aws_config::from_env().region("us-east-1").load().await;
-    // let client = Client::new(&config);
+    let aws_s3_bucket = env::var("AWS_S3_BUCKET").expect("AWS_S3_BUCKET must be set");
+    let config = aws_config::from_env().region("us-east-1").load().await;
+    let client = Client::new(&config);
 
 
     // let result = client
     //    .get_object()
-    //    .bucket(AWS_S3_BUCKET)
+    //    .bucket(aws_s3_bucket)
     //    .key("raw-data/tags2name.csv")
     //    .send()
     //    .await
     //    .expect("Failed to get object");
 
-    // let bytes = result.body.collect().await.unwrap();
+    // let bytes = result.body().collect().await.unwrap();
     // let bytes = bytes.into_bytes();
 
-    // let cursor = std::io::Cursor::new(bytes);
+    // print!("{:?}", bytes);
 
-    // let df = CsvReader::new(cursor).finish().unwrap();
+    let mut person: String = "SELECT * FROM s3object s WHERE s.\"teamName\" = '".to_owned();
+    person.push_str("Arsenal");
+    person.push('\'');
 
-    // println!("{:?}", df);
+    let mut output = client
+        .select_object_content()
+        .bucket(aws_s3_bucket)
+        .key("processed-data/processed_shots.csv")
+        .expression_type(ExpressionType::Sql)
+        .expression(person)
+        .input_serialization(
+            InputSerialization::builder()
+                .csv(
+                    CsvInput::builder()
+                        .file_header_info(FileHeaderInfo::Use)
+                        .build(),
+                )
+                .compression_type(CompressionType::None)
+                .build(),
+        )
+        .output_serialization(
+            OutputSerialization::builder()
+                .csv(CsvOutput::builder().build())
+                .build(),
+        )
+        .send()
+        .await
+        .expect("Failed to select object content");
+
+    // get the results of output and print them
+
+    let mut results = Vec::new();
+
+    // create a byte stream that can then be added to 
 
 
+    while let Ok(Some(event)) = output.payload.recv().await {
+        match event {
+            SelectObjectContentEventStream::Records(records) => {
+                let test = records
+                .payload()
+                .map(|p| std::str::from_utf8(p.as_ref()).unwrap())
+                .unwrap_or("")
+                .to_string()
+                ;
+
+                results.push(test);
+
+                // println!(
+                //     "Record: {}",
+                //     test
+                    
+                // );
+            }
+            SelectObjectContentEventStream::Stats(stats) => {
+                println!("Stats: {:?}", stats.details().unwrap());
+            }
+            SelectObjectContentEventStream::Progress(progress) => {
+                println!("Progress: {:?}", progress.details().unwrap());
+            }
+            SelectObjectContentEventStream::Cont(_) => {
+                println!("Continuation Event");
+            }
+            SelectObjectContentEventStream::End(_) => {
+                println!("End Event");
+            }
+            otherwise => panic!("Unknown event type: {:?}", otherwise),
+        }
+    }
+
+    // convert results to bytes
+    let mut bytes = Vec::new();
+
+    for result in results {
+        bytes.push(result.into_bytes());
+    }
     
-    // TRY WITH DATAFUSION
-    // let s3_file_system = Arc::new(S3FileSystem::default().await);
 
-    // let filename = "s3://data/alltypes_plain.snappy.parquet";
+    let cursor = std::io::Cursor::new(bytes);
+
+    let df = CsvReader::new(cursor).finish().unwrap();
+
+    print!("{:?}", df);
 
 
-    // let config = datafusion::datasource::listing::ListingTableConfig::new(s3_file_system, filename).infer().await?;
 
-    // let table = ListingTable::try_new(config)?;
-
-    // let mut ctx = SessionContext::new();
-
-    // ctx.register_table("tbl", Arc::new(table))?;
-
-    // let df = ctx.sql("SELECT * FROM tbl").await?;
-    // df.show();
 
     HttpServer::new(|| {
         App::new()

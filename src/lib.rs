12d18e830377eaa0852472dedd1a8c14f0d90c6a
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-04-28 15:58:06 +0000

    add s3 polars

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,67 @@
-use plotters::prelude::*;
-use polars::prelude::*;
+// use plotters::prelude::*;
+// use polars::prelude::*;
 
 
-pub fn read_data(path:&str) -> DataFrame {
-    CsvReader::from_path(path).unwrap().finish().unwrap()
-}
+// pub fn read_data(path:&str) -> DataFrame {
+//     CsvReader::from_path(path).unwrap().finish().unwrap()
+// }
 
-pub fn plot(x: Vec<f64>, y: Vec<f64>, img_path:&str) {
-    let data: Vec<(f64, f64)> = x.iter().cloned().zip(y.iter().cloned()).collect();
-    let root_area = BitMapBackend::new(img_path, (700, 400)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
+// pub fn plot(x: Vec<f64>, y: Vec<f64>, img_path:&str) {
+//     let data: Vec<(f64, f64)> = x.iter().cloned().zip(y.iter().cloned()).collect();
+//     let root_area = BitMapBackend::new(img_path, (700, 400)).into_drawing_area();
+//     root_area.fill(&WHITE).unwrap();
 
-    // calculate min and max values for x and y
-    let mut min_x = x[0];
-    let mut max_x = x[0];
-    let mut min_y = y[0];
-    let mut max_y = y[0];
-    for i in 0..x.len() {
-        if x[i] < min_x {
-            min_x = x[i];
-        }
-        if x[i] > max_x {
-            max_x = x[i];
-        }
-        if y[i] < min_y {
-            min_y = y[i];
-        }
-        if y[i] > max_y {
-            max_y = y[i];
-        }
-    }
+//     // calculate min and max values for x and y
+//     let mut min_x = x[0];
+//     let mut max_x = x[0];
+//     let mut min_y = y[0];
+//     let mut max_y = y[0];
+//     for i in 0..x.len() {
+//         if x[i] < min_x {
+//             min_x = x[i];
+//         }
+//         if x[i] > max_x {
+//             max_x = x[i];
+//         }
+//         if y[i] < min_y {
+//             min_y = y[i];
+//         }
+//         if y[i] > max_y {
+//             max_y = y[i];
+//         }
+//     }
 
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 55)
-        .set_label_area_size(LabelAreaPosition::Bottom, 55)
-        .set_label_area_size(LabelAreaPosition::Right, 55)
-        .set_label_area_size(LabelAreaPosition::Top, 55)
-        .caption("Predictions", ("sans-serif", 45))
-        .build_cartesian_2d(min_x..max_x, min_y..max_y)
-        .unwrap();
+//     let mut ctx = ChartBuilder::on(&root_area)
+//         .set_label_area_size(LabelAreaPosition::Left, 55)
+//         .set_label_area_size(LabelAreaPosition::Bottom, 55)
+//         .set_label_area_size(LabelAreaPosition::Right, 55)
+//         .set_label_area_size(LabelAreaPosition::Top, 55)
+//         .caption("Predictions", ("sans-serif", 45))
+//         .build_cartesian_2d(min_x..max_x, min_y..max_y)
+//         .unwrap();
 
-    ctx.configure_mesh().draw().unwrap();
+//     ctx.configure_mesh().draw().unwrap();
 
-    // Draw Scatter Plot
-    ctx.draw_series(data.iter().map(|point| Circle::new(*point, 4, BLUE)))
-        .unwrap();
-    root_area.present().unwrap();
-    println!("Plot finished");
-}
+//     // Draw Scatter Plot
+//     ctx.draw_series(data.iter().map(|point| Circle::new(*point, 4, BLUE)))
+//         .unwrap();
+//     root_area.present().unwrap();
+//     println!("Plot finished");
+// }
 
-pub fn plot_res(path:&str, img_path:&str){
-    let df = read_data(path);
+// pub fn plot_res(path:&str, img_path:&str){
+//     let df = read_data(path);
 
-    let x_vec = df.column("xg").unwrap().f64().unwrap().to_vec();
-    let mut x:Vec<f64> = Vec::new();
-    for i in 0..x_vec.len() {
-        x.push(x_vec[i].unwrap());
-    }
+//     let x_vec = df.column("xg").unwrap().f64().unwrap().to_vec();
+//     let mut x:Vec<f64> = Vec::new();
+//     for i in 0..x_vec.len() {
+//         x.push(x_vec[i].unwrap());
+//     }
 
-    let y_vec = df.column("label_Goal").unwrap().i64().unwrap().to_vec();
-    let mut y:Vec<f64> = Vec::new();
-    for i in 0..y_vec.len() {
-        y.push(y_vec[i].unwrap() as f64);
-    }
-    plot(x, y, img_path);
-}
+//     let y_vec = df.column("label_Goal").unwrap().i64().unwrap().to_vec();
+//     let mut y:Vec<f64> = Vec::new();
+//     for i in 0..y_vec.len() {
+//         y.push(y_vec[i].unwrap() as f64);
+//     }
+//     plot(x, y, img_path);
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 extern crate actix_web;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-// use aws_sdk_s3::types::ByteStream;
-//use aws_sdk_s3::Client;
-//use std::env;
+use std::env;
 //use serde_json::value::Value;
-use std::fs;
+// use std::fs;
+use aws_sdk_s3::{config, Client, Error};
+use polars::prelude::*;
+// use datafusion_objectstore_s3::object_store::s3::S3FileSystem;
+// use std::sync::Arc;
+// use datafusion::datasource::listing::*;
+// use datafusion::prelude::SessionContext;
+
+
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -13,30 +20,35 @@ async fn hello() -> impl Responder {
 
 //function that plots the team results
 #[get("/team_results")]
-async fn team_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
-    const TEAM_RES: &str = "temp_data/model_res_teams.csv";
-    const TEAM_RES_PNG: &str = "team_res.png";
-    final_project::plot_res(TEAM_RES,TEAM_RES_PNG);
+// async fn team_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
+    async fn team_result_plot() -> impl Responder {
+    // const TEAM_RES: &str = "temp_data/model_res_teams.csv";
+    // const TEAM_RES_PNG: &str = "team_res.png";
+    // final_project::plot_res(TEAM_RES,TEAM_RES_PNG);
 
-    // run the plot function and show plot on the actix server
-    let image_data = fs::read(TEAM_RES_PNG)?;
-    Ok(HttpResponse::Ok()
-        .content_type("image/png")
-        .body(image_data))
+    // // run the plot function and show plot on the actix server
+    // let image_data = fs::read(TEAM_RES_PNG)?;
+    // Ok(HttpResponse::Ok()
+    //     .content_type("image/png")
+    //     .body(image_data))
+
+    HttpResponse::Ok().body("Other test!")
 }
 
 //function that plots the player results
 #[get("/player_results")]
-async fn player_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
-    const PLAY_RES: &str = "temp_data/model_res_players.csv";
-    const PLAY_RES_PNG: &str = "player_res.png";
-    final_project::plot_res(PLAY_RES,PLAY_RES_PNG);
+// async fn player_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
+async fn player_result_plot() -> impl Responder {
+    // const PLAY_RES: &str = "temp_data/model_res_players.csv";
+    // const PLAY_RES_PNG: &str = "player_res.png";
+    // final_project::plot_res(PLAY_RES,PLAY_RES_PNG);
 
-    // run the plot function and show plot on the actix server
-    let image_data = fs::read(PLAY_RES_PNG)?;
-    Ok(HttpResponse::Ok()
-        .content_type("image/png")
-        .body(image_data))
+    // // run the plot function and show plot on the actix server
+    // let image_data = fs::read(PLAY_RES_PNG)?;
+    // Ok(HttpResponse::Ok()
+    //     .content_type("image/png")a
+    //     .body(image_data))
+    HttpResponse::Ok().body("Test!")
 }
 
 
@@ -64,32 +76,48 @@ async fn player_result_plot() -> Result<HttpResponse, actix_web::error::ParseErr
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    //let AWS_S3_BUCKET = env::var("AWS_S3_BUCKET").expect("AWS_S3_BUCKET must be set");
-    // let AWS_ACCESS_KEY_ID = env::var("AWS_ACCESS_KEY_ID")
-    // let AWS_SECRET_ACCESS_KEY = env::var("AWS_SECRET_ACCESS_KEY")
-    // let config = aws_config::load_from_env().await;
-    //let config = aws_config::from_env().region("us-east-1").load().await;
-    //let client = Client::new(&config);
+    let AWS_S3_BUCKET = env::var("AWS_S3_BUCKET").expect("AWS_S3_BUCKET must be set");
+    let AWS_ACCESS_KEY_ID = env::var("AWS_ACCESS_KEY_ID");
+    let AWS_SECRET_ACCESS_KEY = env::var("AWS_SECRET_ACCESS_KEY");
+    let config = aws_config::from_env().region("us-east-1").load().await;
+    let client = Client::new(&config);
 
 
+    let result = client
+       .get_object()
+       .bucket(AWS_S3_BUCKET)
+       .key("raw-data/tags2name.csv")
+       .send()
+       .await
+       .expect("Failed to get object");
 
-    //let result = client
-    //    .get_object()
-    //    .bucket(AWS_S3_BUCKET)
-    //    .key("raw-data/tags2name.csv")
-    //    .send()
-    //    .await
-    //    .expect("Failed to get object");
+    let bytes = result.body.collect().await.unwrap();
+    let bytes = bytes.into_bytes();
 
-    // let bytes = result.body.collect().await?.into_bytes();
-    // let thing: Thing = serde_json::from_slice(&bytes).unwrap();
+    let cursor = std::io::Cursor::new(bytes);
 
-    //let res = result
-     //   .unwrap()
-     //   .json::<HashMap<String, Value>>()
-     //   .await;
+    let df = CsvReader::new(cursor).finish().unwrap();
 
-    //print!("{:?}", res.body)
+    println!("{:?}", df);
+
+
+    
+    // TRY WITH DATAFUSION
+    // let s3_file_system = Arc::new(S3FileSystem::default().await);
+
+    // let filename = "s3://data/alltypes_plain.snappy.parquet";
+
+
+    // let config = datafusion::datasource::listing::ListingTableConfig::new(s3_file_system, filename).infer().await?;
+
+    // let table = ListingTable::try_new(config)?;
+
+    // let mut ctx = SessionContext::new();
+
+    // ctx.register_table("tbl", Arc::new(table))?;
+
+    // let df = ctx.sql("SELECT * FROM tbl").await?;
+    // df.show();
 
     HttpServer::new(|| {
         App::new()

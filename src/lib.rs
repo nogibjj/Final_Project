1f90974a8a8ac1c9ce933755546d58f4458dfe2d
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-04-28 21:00:37 +0000

    add team name endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,94 +1,185 @@
-// use plotters::prelude::*;
-// use polars::prelude::*;
-// use std::env;
-// use aws_sdk_s3::Client;
+use plotters::prelude::*;
+use polars::prelude::*;
+use std::env;
+use aws_sdk_s3::types::{
+    CompressionType, CsvInput, CsvOutput, ExpressionType, FileHeaderInfo, InputSerialization,
+    OutputSerialization, SelectObjectContentEventStream,
+};
+use aws_sdk_s3::Client;
 
 
-// pub async fn read_data(path:&str) -> DataFrame {
-//     // CsvReader::from_path(path).unwrap().finish().unwrap()
-//     let AWS_S3_BUCKET = env::var("AWS_S3_BUCKET").expect("AWS_S3_BUCKET must be set");
-//     let AWS_ACCESS_KEY_ID = env::var("AWS_ACCESS_KEY_ID");
-//     let AWS_SECRET_ACCESS_KEY = env::var("AWS_SECRET_ACCESS_KEY");
-//     let config = aws_config::from_env().region("us-east-1").load().await;
-//     let client = Client::new(&config);
+pub async fn read_data(path:&str) -> DataFrame {
+    // CsvReader::from_path(path).unwrap().finish().unwrap()
+    let aws_s3_bucket: String = env::var("AWS_S3_BUCKET").expect("AWS_S3_BUCKET must be set");
+    let config = aws_config::from_env().region("us-east-1").load().await;
+    let client = Client::new(&config);
 
 
-//     let result = client
-//        .get_object()
-//        .bucket(AWS_S3_BUCKET)
-//        .key(path)
-//        .send()
-//        .await
-//        .expect("Failed to get object");
+    let result = client
+       .get_object()
+       .bucket(aws_s3_bucket)
+       .key(path)
+       .send()
+       .await
+       .expect("Failed to get object");
 
-//     let bytes = result.body.collect().await.unwrap();
-//     let bytes = bytes.into_bytes();
+    let bytes = result.body.collect().await.unwrap();
+    let bytes = bytes.into_bytes();
 
-//     let cursor = std::io::Cursor::new(bytes);
+    let cursor = std::io::Cursor::new(bytes);
 
-//     CsvReader::new(cursor).finish().unwrap()
+    CsvReader::new(cursor).finish().unwrap()
 
-
-// }
-
-
-// pub fn plot(x: Vec<f64>, y: Vec<f64>, img_path:&str) {
-//     let data: Vec<(f64, f64)> = x.iter().cloned().zip(y.iter().cloned()).collect();
-//     let root_area = BitMapBackend::new(img_path, (700, 400)).into_drawing_area();
-//     root_area.fill(&WHITE).unwrap();
-
-//     // calculate min and max values for x and y
-//     let mut min_x = x[0];
-//     let mut max_x = x[0];
-//     let mut min_y = y[0];
-//     let mut max_y = y[0];
-//     for i in 0..x.len() {
-//         if x[i] < min_x {
-//             min_x = x[i];
-//         }
-//         if x[i] > max_x {
-//             max_x = x[i];
-//         }
-//         if y[i] < min_y {
-//             min_y = y[i];
-//         }
-//         if y[i] > max_y {
-//             max_y = y[i];
-//         }
-//     }
-
-//     let mut ctx = ChartBuilder::on(&root_area)
-//         .set_label_area_size(LabelAreaPosition::Left, 55)
-//         .set_label_area_size(LabelAreaPosition::Bottom, 55)
-//         .set_label_area_size(LabelAreaPosition::Right, 55)
-//         .set_label_area_size(LabelAreaPosition::Top, 55)
-//         .caption("Predictions", ("sans-serif", 45))
-//         .build_cartesian_2d(min_x..max_x, min_y..max_y)
-//         .unwrap();
-
-//     ctx.configure_mesh().draw().unwrap();
-
-//     // Draw Scatter Plot
-//     ctx.draw_series(data.iter().map(|point| Circle::new(*point, 4, BLUE)))
-//         .unwrap();
-//     root_area.present().unwrap();
-//     println!("Plot finished");
-// }
+}
 
 
-// pub async fn plot_res(path:&str, img_path:&str){
-//     let df = read_data(path).await;
+async fn get_queried_bytes(query:&String, path:&str, header:FileHeaderInfo) -> Vec<u8>{
+    let aws_s3_bucket = env::var("AWS_S3_BUCKET").expect("AWS_S3_BUCKET must be set");
+    let config = aws_config::from_env().region("us-east-1").load().await;
+    let client = Client::new(&config);
 
-//     let x_vec = df.column("xg").unwrap().f64().unwrap().to_vec();
-//     let mut x:Vec<f64> = Vec::new();
-//     for i in 0..x_vec.len() {
-//         x.push(x_vec[i].unwrap());
-//     }
+    let mut output = client
+        .select_object_content()
+        .bucket(aws_s3_bucket)
+        .key(path)
+        .expression_type(ExpressionType::Sql)
+        .expression(query)
+        .input_serialization(
+            InputSerialization::builder()
+                .csv(
+                    CsvInput::builder()
+                        .file_header_info(header)
+                        .build(),
+                )
+                .compression_type(CompressionType::None)
+                .build(),
+        )
+        .output_serialization(
+            OutputSerialization::builder()
+                .csv(CsvOutput::builder().build())
+                .build(),
+        )
+        .send()
+        .await
+        .expect("Failed to select object content");
 
-//     let y_vec = df.column("label_Goal").unwrap().i64().unwrap().to_vec();
-//     let mut y:Vec<f64> = Vec::new();
-//     for i in 0..y_vec.len() {
-//         y.push(y_vec[i].unwrap() as f64);
-//     }
-//     plot(x, y, img_path);
-// }
+
+    // get the results of output and print them
+    let mut results = Vec::new();
+
+
+    while let Ok(Some(event)) = output.payload.recv().await {
+        match event {
+            SelectObjectContentEventStream::Records(records) => {
+                let res = records
+                .payload()
+                .map(|p| std::str::from_utf8(p.as_ref()).unwrap())
+                .unwrap_or("")
+                .to_string()
+                ;
+
+                results.push(res);
+
+            }
+            SelectObjectContentEventStream::Stats(stats) => {
+                println!("Stats: {:?}", stats.details().unwrap());
+            }
+            SelectObjectContentEventStream::Progress(progress) => {
+                println!("Progress: {:?}", progress.details().unwrap());
+            }
+            SelectObjectContentEventStream::Cont(_) => {
+                println!("Continuation Event");
+            }
+            SelectObjectContentEventStream::End(_) => {
+                println!("End Event");
+            }
+            otherwise => panic!("Unknown event type: {:?}", otherwise),
+        }
+    }
+
+
+    // convert results to bytes
+    let mut bytes = Vec::<u8>::new();
+    // convert results to bytes and store in test 
+    for i in results {
+        let temp = i.as_bytes().to_vec();
+        bytes.extend(temp);
+    }
+
+    bytes
+}
+
+
+pub async fn get_queried_data(query:String, path:&str) -> DataFrame {
+    let data_bytes = get_queried_bytes(&query, path, FileHeaderInfo::Use).await;
+    let header_query: &str = "SELECT * FROM s3object s LIMIT 1";
+    let header_bytes = get_queried_bytes(&query, header_query, FileHeaderInfo::None).await;
+
+    // Concat header with data 
+    let final_bytes = [header_bytes, data_bytes].concat();
+
+    let cursor = std::io::Cursor::new(final_bytes);
+    CsvReader::new(cursor).finish().unwrap()
+}
+
+
+pub fn plot(x: Vec<f64>, y: Vec<f64>, img_path:&str) {
+    let data: Vec<(f64, f64)> = x.iter().cloned().zip(y.iter().cloned()).collect();
+    let root_area = BitMapBackend::new(img_path, (700, 400)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    // calculate min and max values for x and y
+    let mut min_x = x[0];
+    let mut max_x = x[0];
+    let mut min_y = y[0];
+    let mut max_y = y[0];
+    for i in 0..x.len() {
+        if x[i] < min_x {
+            min_x = x[i];
+        }
+        if x[i] > max_x {
+            max_x = x[i];
+        }
+        if y[i] < min_y {
+            min_y = y[i];
+        }
+        if y[i] > max_y {
+            max_y = y[i];
+        }
+    }
+
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 55)
+        .set_label_area_size(LabelAreaPosition::Bottom, 55)
+        .set_label_area_size(LabelAreaPosition::Right, 55)
+        .set_label_area_size(LabelAreaPosition::Top, 55)
+        .caption("Predictions", ("sans-serif", 45))
+        .build_cartesian_2d(min_x..max_x, min_y..max_y)
+        .unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
+    // Draw Scatter Plot
+    ctx.draw_series(data.iter().map(|point| Circle::new(*point, 4, BLUE)))
+        .unwrap();
+    root_area.present().unwrap();
+    println!("Plot finished");
+}
+
+
+pub async fn plot_res(path:&str, img_path:&str){
+    let df = read_data(path).await;
+
+    let x_vec = df.column("xg").unwrap().f64().unwrap().to_vec();
+    let mut x:Vec<f64> = Vec::new();
+    for i in 0..x_vec.len() {
+        x.push(x_vec[i].unwrap());
+    }
+
+    let y_vec = df.column("label_Goal").unwrap().i64().unwrap().to_vec();
+    let mut y:Vec<f64> = Vec::new();
+    for i in 0..y_vec.len() {
+        y.push(y_vec[i].unwrap() as f64);
+    }
+    plot(x, y, img_path);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 extern crate actix_web;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use final_project::get_queried_data;
 
-// use std::fs;
-// use aws_sdk_s3::Client;
-use std::env;
+use std::fs;
 
-use aws_sdk_s3::types::{
-    CompressionType, CsvInput, CsvOutput, ExpressionType, FileHeaderInfo, InputSerialization,
-    OutputSerialization, SelectObjectContentEventStream,
-};
-use aws_sdk_s3::Client;
-
-use polars::prelude::*;
 
 
 
@@ -23,191 +15,64 @@ async fn hello() -> impl Responder {
 
 //function that plots the team results
 #[get("/team_results")]
-// async fn team_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
-    async fn team_result_plot() -> impl Responder {
-    // const TEAM_RES: &str = "processed-data/model_res_teams.csv";
-    // const TEAM_RES_PNG: &str = "team_res.png";
-    // final_project::plot_res(TEAM_RES,TEAM_RES_PNG).await;
+async fn team_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
+    // async fn team_result_plot() -> impl Responder {
+    const TEAM_RES: &str = "processed-data/model_res_teams.csv";
+    const TEAM_RES_PNG: &str = "team_res.png";
+    final_project::plot_res(TEAM_RES,TEAM_RES_PNG).await;
 
-    // // run the plot function and show plot on the actix server
-    // let image_data = fs::read(TEAM_RES_PNG)?;
-    // Ok(HttpResponse::Ok()
-    //     .content_type("image/png")
-    //     .body(image_data))
+    // run the plot function and show plot on the actix server
+    let image_data = fs::read(TEAM_RES_PNG)?;
+    Ok(HttpResponse::Ok()
+        .content_type("image/png")
+        .body(image_data))
 
-    HttpResponse::Ok().body("Other test!")
+    // HttpResponse::Ok().body("Other test!")
 }
 
 //function that plots the player results
 #[get("/player_results")]
-// async fn player_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
-async fn player_result_plot() -> impl Responder {
-    // const PLAY_RES: &str = "processed-data/model_res_players.csv";
-    // const PLAY_RES_PNG: &str = "player_res.png";
-    // final_project::plot_res(PLAY_RES,PLAY_RES_PNG).await;
+async fn player_result_plot() -> Result<HttpResponse, actix_web::error::ParseError> {
+// async fn player_result_plot() -> impl Responder {
+    const PLAY_RES: &str = "processed-data/model_res_players.csv";
+    const PLAY_RES_PNG: &str = "player_res.png";
+    final_project::plot_res(PLAY_RES,PLAY_RES_PNG).await;
 
-    // // run the plot function and show plot on the actix server
-    // let image_data = fs::read(PLAY_RES_PNG)?;
-    // Ok(HttpResponse::Ok()
-    //     .content_type("image/png")
-    //     .body(image_data))
-    HttpResponse::Ok().body("Test!")
+    // run the plot function and show plot on the actix server
+    let image_data = fs::read(PLAY_RES_PNG)?;
+    Ok(HttpResponse::Ok()
+        .content_type("image/png")
+        .body(image_data))
+//     HttpResponse::Ok().body("Test!")
 }
 
 
-// #[get("/team/{team_name}")]
-// async fn team() -> impl Responder {
-//     HttpResponse::Ok().body("Hello!")
-// }
-
-// #[get("/player/{player_name}")]
-// async fn player() -> impl Responder {
-//     HttpResponse::Ok().body("Other test!")
-// }
-
-async fn get_queried_data(query:String, path:&str, header:FileHeaderInfo) -> Vec<u8>{
-    let aws_s3_bucket = env::var("AWS_S3_BUCKET").expect("AWS_S3_BUCKET must be set");
-    let config = aws_config::from_env().region("us-east-1").load().await;
-    let client = Client::new(&config);
-
-    let mut output = client
-        .select_object_content()
-        .bucket(aws_s3_bucket)
-        .key(path)
-        .expression_type(ExpressionType::Sql)
-        .expression(query)
-        .input_serialization(
-            InputSerialization::builder()
-                .csv(
-                    CsvInput::builder()
-                        .file_header_info(header)
-                        .build(),
-                )
-                .compression_type(CompressionType::None)
-                .build(),
-        )
-        .output_serialization(
-            OutputSerialization::builder()
-                .csv(CsvOutput::builder().build())
-                .build(),
-        )
-        .send()
-        .await
-        .expect("Failed to select object content");
+#[get("/team/{team_name}")]
+async fn team_specific_data(team_name: web::Path<String>) -> impl Responder {
+    const TEAM_RES: &str = "processed-data/processed_shots.csv";
 
 
-    // get the results of output and print them
-    let mut results = Vec::new();
+    let mut team: String = "SELECT * FROM s3object s WHERE s.\"teamName\" = '".to_owned();
+    team.push_str(team_name.to_string().as_str());
+    team.push('\'');
+
+    let team_df = get_queried_data(team, TEAM_RES);
 
 
-    while let Ok(Some(event)) = output.payload.recv().await {
-        match event {
-            SelectObjectContentEventStream::Records(records) => {
-                let res = records
-                .payload()
-                .map(|p| std::str::from_utf8(p.as_ref()).unwrap())
-                .unwrap_or("")
-                .to_string()
-                ;
-
-                results.push(res);
-
-            }
-            SelectObjectContentEventStream::Stats(stats) => {
-                println!("Stats: {:?}", stats.details().unwrap());
-            }
-            SelectObjectContentEventStream::Progress(progress) => {
-                println!("Progress: {:?}", progress.details().unwrap());
-            }
-            SelectObjectContentEventStream::Cont(_) => {
-                println!("Continuation Event");
-            }
-            SelectObjectContentEventStream::End(_) => {
-                println!("End Event");
-            }
-            otherwise => panic!("Unknown event type: {:?}", otherwise),
-        }
-    }
-
-
-    // convert results to bytes
-    let mut bytes = Vec::<u8>::new();
-    // convert results to bytes and store in test 
-    for i in results {
-        let temp = i.as_bytes().to_vec();
-        bytes.extend(temp);
-    }
-
-
-    bytes
+    HttpResponse::Ok().body("Hello!")
 }
+
+
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    
-
-
-    // let result = client
-    //    .get_object()
-    //    .bucket(aws_s3_bucket)
-    //    .key("raw-data/tags2name.csv")
-    //    .send()
-    //    .await
-    //    .expect("Failed to get object");
-
-    // let bytes = result.body().collect().await.unwrap();
-    // let bytes = bytes.into_bytes();
-
-    // print!("{:?}", bytes);
-
-    // QUERIES
-    // team name query
-    let mut team: String = "SELECT * FROM s3object s WHERE s.\"teamName\" = '".to_owned();
-    team.push_str("Arsenal");
-    team.push('\'');
-
-    // header query 
-    let header: String = "SELECT * FROM s3object s LIMIT 1".to_owned();
-
-    // player name query 
-    let player_name = "Lionel Messi";
-    // separate first and last name from player_name
-    let mut split = player_name.split_whitespace();
-    let first_name = split.next().unwrap();
-    let last_name = split.next().unwrap();
-
-    // player query
-    let mut player: String = "SELECT * FROM s3object s WHERE s.\"firstName\" = '".to_owned();
-    player.push_str(first_name);
-    player.push_str("' AND s.\"lastName\" = '");
-    player.push_str(last_name);
-    player.push('\'');
-
-
-
-    let test = "processed-data/processed_shots.csv";
-    // GET DATA
-    let data_bytes = get_queried_data(player, test, FileHeaderInfo::Use).await;
-    let header_bytes = get_queried_data(header, test, FileHeaderInfo::None).await;
-
-    // Concat header with data 
-    let final_bytes = [header_bytes, data_bytes].concat();
-
-    let cursor = std::io::Cursor::new(final_bytes);
-    let final_df = CsvReader::new(cursor).finish().unwrap();
-
-
-    print!("{:?}", final_df);
-
-
-
-
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(team_result_plot)
             .service(player_result_plot)
+            .service(team_specific_data)
     })
     .bind("0.0.0.0:8080")?
     .run()
